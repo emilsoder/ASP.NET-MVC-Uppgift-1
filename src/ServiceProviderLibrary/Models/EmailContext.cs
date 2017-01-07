e6242f5a4bd8888560b726d804ff453a4babd402
Emil H. Södergren <sodergren.emil@outlook.com>
2017-01-07 13:02:38 +0100

    --

---
+++ src/ServiceProviderLibrary/Models/EmailContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServiceProviderLibrary.Models
+{
+    public class EmailContext
+    {
+    }
+}
+
+// Scaffold-DbContext "Server=(localdb)\MSSQLLocalDB;Database=InfoServiceDB;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
